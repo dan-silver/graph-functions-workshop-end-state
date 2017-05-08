9c8cff81957a0e602d0bdccc2805a093a869f0ec
Dan Silver <dannysilver3@gmail.com>
2017-05-08 15:40:51 -0700

    Update secrets.ts

--- secrets.ts
+++ secrets.ts
@@ -1,7 +1,7 @@
 // Register your app at https://apps.dev.microsoft.com
 
-export const MicrosoftAppID = "2299e6f3-9329-4142-a0e6-581261d5ea62";
+export const MicrosoftAppID = "";
 
-export const MicrosoftAppSecret = "9op9EXNtS9r2krqHbRBT9oP"; // Click 'Generate new password'
+export const MicrosoftAppSecret = ""; // Click 'Generate new password'
 
-export const TenantDomain = "MOD789932.onmicrosoft.com"; // possibly something like MOD127192.onmicrosoft.com
+export const TenantDomain = ""; // possibly something like MOD127192.onmicrosoft.com
